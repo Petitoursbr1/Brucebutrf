ca229d296ed4b545d38a561fb8852007c7973a84
Petitoursbr1 <130918045+Petitoursbr1@users.noreply.github.com>
2024-06-25 18:37:24 +0200

    Update rf.cpp

--- src/rf.cpp
+++ src/rf.cpp
@@ -12,8 +12,7 @@
 // PCA9554 extIo1(pca9554pw_address);
 
 #define RMT_RX_CHANNEL  RMT_CHANNEL_6
-#define RMT_BLOCK_NUM
-
+#define RMT_TX_CHANNEL  RMT_CHANNEL_0
 
 #define RMT_CLK_DIV   80 /*!< RMT counter clock divider */
 #define RMT_1US_TICKS (80000000 / RMT_CLK_DIV / 1000000)
@@ -24,6 +23,9 @@
 #define DISPLAY_HEIGHT 130 // Height of the display area for the waveform
 #define DISPLAY_WIDTH  240 // Width of the display area
 #define LINE_WIDTH 2 // Adjust line width as needed
+
+rmt_item32_t* recorded_signal = nullptr;
+size_t recorded_size = 0;
 
 void initRMT() {
     rmt_config_t rxconfig;
@@ -40,6 +42,22 @@ void initRMT() {
     ESP_ERROR_CHECK(rmt_driver_install(rxconfig.channel, 2048, 0));
 }
 
+void initRMT_TX() {
+    rmt_config_t txconfig;
+    txconfig.rmt_mode = RMT_MODE_TX;
+    txconfig.channel = RMT_TX_CHANNEL;
+    txconfig.gpio_num = gpio_num_t(RfTx);
+    txconfig.mem_block_num = 1;
+    txconfig.clk_div = RMT_CLK_DIV;
+    txconfig.tx_config.loop_en = false;
+    txconfig.tx_config.carrier_en = false;
+    txconfig.tx_config.idle_output_en = true;
+    txconfig.tx_config.idle_level = RMT_IDLE_LEVEL_LOW;
+
+    ESP_ERROR_CHECK(rmt_config(&txconfig));
+    ESP_ERROR_CHECK(rmt_driver_install(txconfig.channel, 0));
+}
+
 bool sendRF = false;
 
 void rf_spectrum() { //@IncursioHack - https://github.com/IncursioHack ----thanks @aat440hz - RF433ANY-M5Cardputer
@@ -51,43 +69,108 @@ void rf_spectrum() { //@IncursioHack - https://github.com/IncursioHack ----thank
     pinMode(RfRx, INPUT);
     initRMT();
 
-        RingbufHandle_t rb = nullptr;
-        rmt_get_ringbuf_handle(RMT_RX_CHANNEL, &rb);
-        rmt_rx_start(RMT_RX_CHANNEL, true);
-        while (rb) {
-            size_t rx_size = 0;
-            rmt_item32_t* item = (rmt_item32_t*)xRingbufferReceive(rb, &rx_size, 500);
-            if (item != nullptr) {
-                if (rx_size != 0) {
-                    // Clear the display area
-                    tft.fillRect(0, 20, DISPLAY_WIDTH, DISPLAY_HEIGHT, TFT_BLACK);
-                    // Draw waveform based on signal strength
-                    for (size_t i = 0; i < rx_size; i++) {
-                        int lineHeight = map(item[i].duration0 + item[i].duration1, 0, SIGNAL_STRENGTH_THRESHOLD, 0, DISPLAY_HEIGHT/2);
-                        int lineX = map(i, 0, rx_size - 1, 0, DISPLAY_WIDTH - 1); // Map i to within the display width
-                        // Ensure drawing coordinates stay within the box bounds
-                        int startY = constrain(20 + DISPLAY_HEIGHT / 2 - lineHeight / 2, 20, 20 + DISPLAY_HEIGHT);
-                        int endY = constrain(20 + DISPLAY_HEIGHT / 2 + lineHeight / 2, 20, 20 + DISPLAY_HEIGHT);
-                        tft.drawLine(lineX, startY, lineX, endY, TFT_PURPLE);
-                    }
+    RingbufHandle_t rb = nullptr;
+    rmt_get_ringbuf_handle(RMT_RX_CHANNEL, &rb);
+    rmt_rx_start(RMT_RX_CHANNEL, true);
+    while (rb) {
+        size_t rx_size = 0;
+        rmt_item32_t* item = (rmt_item32_t*)xRingbufferReceive(rb, &rx_size, 500);
+        if (item != nullptr) {
+            if (rx_size != 0) {
+                // Clear the display area
+                tft.fillRect(0, 20, DISPLAY_WIDTH, DISPLAY_HEIGHT, TFT_BLACK);
+                // Draw waveform based on signal strength
+                for (size_t i = 0; i < rx_size; i++) {
+                    int lineHeight = map(item[i].duration0 + item[i].duration1, 0, SIGNAL_STRENGTH_THRESHOLD, 0, DISPLAY_HEIGHT/2);
+                    int lineX = map(i, 0, rx_size - 1, 0, DISPLAY_WIDTH - 1); // Map i to within the display width
+                    // Ensure drawing coordinates stay within the box bounds
+                    int startY = constrain(20 + DISPLAY_HEIGHT / 2 - lineHeight / 2, 20, 20 + DISPLAY_HEIGHT);
+                    int endY = constrain(20 + DISPLAY_HEIGHT / 2 + lineHeight / 2, 20, 20 + DISPLAY_HEIGHT);
+                    tft.drawLine(lineX, startY, lineX, endY, TFT_PURPLE);
                 }
-                vRingbufferReturnItem(rb, (void*)item);
             }
-                    if (checkEscPress()) {
-                    rmt_rx_stop(RMT_RX_CHANNEL);
-                    returnToMenu=true;
-                    break;
-                }
+            vRingbufferReturnItem(rb, (void*)item);
         }
-            // Checks para sair do while
-
+        if (checkEscPress()) {
+            rmt_rx_stop(RMT_RX_CHANNEL);
+            returnToMenu=true;
+            break;
+        }
+    }
     rmt_rx_stop(RMT_RX_CHANNEL);
     delay(10);
-
-
-
 }
 
+void rf_record() {
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(1);
+    tft.println("");
+    tft.println("  RF433 - Record");
+    pinMode(RfRx, INPUT);
+    initRMT();
+
+    RingbufHandle_t rb = nullptr;
+    rmt_get_ringbuf_handle(RMT_RX_CHANNEL, &rb);
+    rmt_rx_start(RMT_RX_CHANNEL, true);
+    recorded_signal = nullptr;
+    recorded_size = 0;
+    while (rb) {
+        size_t rx_size = 0;
+        rmt_item32_t* item = (rmt_item32_t*)xRingbufferReceive(rb, &rx_size, 500);
+        if (item != nullptr) {
+            if (rx_size != 0) {
+                // Allocate memory and copy the received items
+                recorded_signal = (rmt_item32_t*)malloc(rx_size);
+                if (recorded_signal) {
+                    memcpy(recorded_signal, item, rx_size);
+                    recorded_size = rx_size / sizeof(rmt_item32_t);
+                }
+            }
+            vRingbufferReturnItem(rb, (void*)item);
+        }
+        if (checkEscPress()) {
+            rmt_rx_stop(RMT_RX_CHANNEL);
+            returnToMenu = true;
+            break;
+        }
+    }
+    rmt_rx_stop(RMT_RX_CHANNEL);
+    delay(10);
+    if (recorded_signal) {
+        tft.println("Recording complete.");
+    } else {
+        tft.println("No signal recorded.");
+    }
+}
+
+void rf_replay() {
+    if (!recorded_signal || recorded_size == 0) {
+        tft.fillScreen(TFT_BLACK);
+        tft.setTextSize(2);
+        tft.println("");
+        tft.println("  RF433 - Replay");
+        tft.println("");
+        tft.println("No signal recorded.");
+        delay(2000);
+        return;
+    }
+
+    tft.fillScreen(TFT_BLACK);
+    tft.setTextSize(2);
+    tft.println("");
+    tft.println("  RF433 - Replay");
+    tft.println("");
+    tft.println("Replaying signal...");
+    
+    pinMode(RfTx, OUTPUT);
+    initRMT_TX();
+
+    ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, recorded_signal, recorded_size, true));
+    ESP_ERROR_CHECK(rmt_wait_tx_done(RMT_TX_CHANNEL, portMAX_DELAY));
+
+    tft.println("Replay complete.");
+    delay(2000);
+}
 
 void rf_jammerFull() { //@IncursioHack - https://github.com/IncursioHack -  thanks @EversonPereira - rfcardputer
     pinMode(RfTx, OUTPUT);
@@ -110,7 +193,6 @@ void rf_jammerFull() { //@IncursioHack - https://github.com/IncursioHack -  than
     }
     digitalWrite(RfTx, LOW); // Turn Jammer OFF
 }
-
 
 void rf_jammerIntermittent() { //@IncursioHack - https://github.com/IncursioHack -  thanks @EversonPereira - rfcardputer
     pinMode(RfTx, OUTPUT);
